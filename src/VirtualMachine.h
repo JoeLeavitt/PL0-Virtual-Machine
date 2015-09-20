20396ccd03fa895530c45a17ebb5f846d89e3c18
Joe <joeleav1@gmail.com>
2015-09-20 06:33:09 -0400

    Fix function declarations

--- src/VirtualMachine.h
+++ src/VirtualMachine.h
@@ -1,19 +1,21 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-/* Struct representing the Execute cycle */
-typedef struct instructions
+#define MAX_STACK_HEIGHT 200
+#define MAX_CODE_LENGTH 500
+#define MAX_LINE_LENGTH 10
+#define MAX_LEXI_LEVEL 3
+
+typedef struct Execute_Cycle_Instructions
 {
     int OP; // Opcode
     int L;  // Lexicographical Level
     int M;  // Parameter
-    int R;  // Register
-} instructions;
+} instruction;
 
-/* Function Definitions */
-void printCodes(FILE *ofp, int code[][MAX_CODE_LENGTH], int codeLen);
-instructions fetch(int PC, int code[][MAX_CODE_LENGTH]);
+void read(FILE *ifp, instruction *instructions);
+void printInstructions(FILE *ofp, instruction* instructions);
+void printStacktrace(int prevPC, instruction *IR, int PC, int BP, int SP, int *stack, FILE *ofp)
 int base(int l, int base, int *stack);
 char *getOpcode(int OP);
-
